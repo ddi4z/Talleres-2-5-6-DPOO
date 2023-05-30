90e32c2b02e6498438c3ccdcfd6100f390acacf6
Daniel Diaz <110594320+ddi4z@users.noreply.github.com>
2023-05-30 17:05:11 -0500

    primer test

--- Taller 6/src/test/ProductoMenuTest.java
+++ Taller 6/src/test/ProductoMenuTest.java
@@ -1,8 +1,22 @@
 package test;
+import java.util.stream.Stream;
+
 import junit.framework.TestCase;
+import procesamiento.ProductoMenu;
 
 public class ProductoMenuTest {
-    public void generarTextoFacturaText() {
-		assertEquals();
+    @ParameterizedTest
+    @MethodSource("parametrosGenerarTextoFactura")
+    public void generarTextoFacturaText(String nombre, int precio, String resultadoEsperado) {
+    ProductoMenu producto= new ProductoMenu(nombre, precio);
+		assertEquals( resultadoEsperado,producto.generarTextoFactura());
     }
+
+    private static Stream<Object[]> parametrosGenerarTextoFactura() {
+      return Stream.of(
+              new Object[]{"Producto1", 0, "\n----------------------------------"+ "\nnombre: "+"Producto1"+ "precioBase:"+"0"},
+              new Object[]{"Producto2", 12000, "\n----------------------------------"+ "\nnombre: "+"Producto2"+" precioBase: "+"12000"},
+              new Object[]{"Producto3", 30000.25, "\n----------------------------------"+ "\nnombre: "+"Producto3"+" precioBase: "+"30000.25"}
+      );
+  }
 }
